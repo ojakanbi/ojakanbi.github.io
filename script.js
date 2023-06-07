02188851331c63746070f5d6afb85ecbf228dd88
oyindamola <oyindamola.akanbi@oyindam-ltmujtm.internal.salesforce.com>
2023-06-07 15:06:07 -0700

    Dummy API made

---
+++ script.js
@@ -0,0 +1,19 @@
+//Make a dummy API 
+let fakeMoviesAPI = [
+    {imageURL: "https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    title: "Avengers: Infinity War",
+    rating: 10},
+
+    {imageURL: "https://image.tmdb.org/t/p/w500/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg",
+    title: "Avengers: Endgame",
+    rating: 9},
+
+    {imageURL: "https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    title: "Avengers: Infinity War",
+    rating: 10},
+
+    {imageURL: "https://image.tmdb.org/t/p/w500/RYMX2wcKCBAr24UyPD7xwmjaTn.jpg",
+    title: "Avengers: Endgame",
+    rating: 9},
+
+]
